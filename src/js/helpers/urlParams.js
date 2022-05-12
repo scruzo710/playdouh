c4015a6d3e9e35f0d16c5656b624599948ed49a6
Sergio Cruz <sergiosco.93@gmail.com>
2022-05-11 18:26:18 -0600

    initial project setup

--- src/js/helpers/urlParams.js
+++
@@ -1,8 +0,0 @@
-const getUrlParams = () => new URLSearchParams(window.location.search)
-
-const getRedirectUrl = () => {
-  const urlParams = getUrlParams()
-  return urlParams.has('redirect_url') ? urlParams.get('redirect_url') : '/'
-}
-
-export default { getUrlParams, getRedirectUrl }
